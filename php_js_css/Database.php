ce08aac150e518f199180e3f4e5c0425cb976954
blendaarama <br73022@ubt-uni.net>
2026-04-28 21:00:12 +0200

    name change on database

--- php_js_css/Database.php
+++ php_js_css/Database.php
@@ -1,7 +1,7 @@
 <?php
 class Database {
     private $host = "localhost";
-    private $db_name = "restaurant_booking";
+    private $db_name = " restaurant_booking";
     private $username = "root";
     private $password = "";
     public $conn;
